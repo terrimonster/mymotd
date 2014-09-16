3ca8bd682f7170c2689d2c17cc55c4988a5dc48f
Terri Haber <terri@puppetlabs.com>
2014-09-16 16:58:07 -0700

    fix error and lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class mymotd {
   file { '/etc/motd':
-    ensure => file
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
     content => template('mymotd/motd.erb'),
 }
